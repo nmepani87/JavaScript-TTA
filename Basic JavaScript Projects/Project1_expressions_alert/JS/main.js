2b32f7ca66da3d16f3532669804d23241b96f759
nmepani87 <n_mepani@yahoo.co.uk>
2022-07-16 20:56:17 +0100

    1 Assignment part

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+
+window.alert('Hello there') //window alert to great user
+
+var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string"; //Two variables given strings to be concatenated
+
+document.write((Sent1) + (Sent2)); //2 variables concatenated
+document.write(" <br> "); //Used <br> as couldn't figure out easier way as document.write not recommended to use stil
+document.write(9*9); // Example of an expression printed
+
+
